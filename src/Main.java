f785956ccb58ab91ee1002795642c56bb16bfc99
Eunil <eunilcarl02@gmail.com>
2024-06-16 11:55:26 +0800

    lul

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
-public class Main{
-    public static void main (String[] args){
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JButton;
 
+
+public class Main {
+    public static void main(String[] args){
+
+        JFrame f = new JFrame();
+        JLabel l = new JLabel();
+        JButton b = new JButton();
+
+        l.setText("Lhance tuazon");
+        b.setText("Okay");
+
+        f.add(l);
+        f.add(b);
+
+        f.setVisible(true);
+        f.setSize(500,500);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
