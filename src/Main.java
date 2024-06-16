2cdce1b7b6bf9ddeb2a8b49ee5088a27db2b258e
Eunil <eunilcarl02@gmail.com>
2024-06-16 11:50:00 +0800

    hi po

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Main {
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+
+
+public class Main {
+    public static void main(String[] args){
+
+        JFrame f = new JFrame();
+        JLabel l = new JLabel();
+        JButton b = new JButton();
+
+        l.setText("Lhance tuazon");
+        b.setText("Okay");
+
+        f.add(l);
+        f.add(b);
+
+        f.setVisible(true);
+        f.setSize(500,500);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
 }
