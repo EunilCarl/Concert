bbcb29df8ff51848a5b764f3f329e9b2a4073222
Eunil <eunilcarl02@gmail.com>
2024-06-18 17:21:52 +0800

    fixed Concert.java

--- src/Concert.java
+++ src/Concert.java
@@ -7,59 +7,83 @@ public class Concert extends JFrame implements ActionListener {
     JButton b;
     JPanel mainP;
     JPanel p1;
+
+    //Header
     JPanel head;
+    JLabel headLab;
+
+    //Buttons
     java.util.List<JButton> buttons;
 
     Concert() {
         head = new JPanel();
         mainP = new JPanel();
-        p1 = new JPanel();
+        headLab = new JLabel();
 
+        head.setLayout(new FlowLayout(FlowLayout.LEFT));
+
+        headLab.setText("BPSU Concert");
+        headLab.setFont(new Font(null,Font.PLAIN,50));
 
         mainP.setBackground(new Color(204,102,0));
-        mainP.setPreferredSize(new Dimension(300,800));
         mainP.setLayout(new FlowLayout());
-
-        p1.setPreferredSize(new Dimension(1800,900));
-        mainP.add(p1);
-
+        mainP.setPreferredSize(new Dimension(0,2500));
 
         buttons = new java.util.ArrayList<>();
         this.setLayout(new BorderLayout());
-        this.setSize(1900, 1000);
+        this.setSize(1900, 900);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setVisible(true);
-        this.add(mainP, BorderLayout.SOUTH);
+
+        JScrollPane scroller = new JScrollPane(mainP);
+
+        head.add(headLab);
+        head.setPreferredSize(new Dimension(1000,80));
+        head.setBackground(Color.PINK);
+
+        //add to Frame
+        this.add(head,BorderLayout.NORTH);
+        this.add(scroller, BorderLayout.CENTER);
+
+
+
 
     }
 
     public void setComp(String title, String loc) {
 
+        p1 = new JPanel();
 
         b = new JButton();
         b.setText("Buy Tickets");
         b.addActionListener(this);
+        b.setPreferredSize(new Dimension(250,40));
+
         b.setFocusable(false);
 
         buttons.add(b);
 
 
         ImageIcon img = new ImageIcon(loc);
-        Image resized = img.getImage().getScaledInstance(200, 250, Image.SCALE_SMOOTH);
+        Image resized = img.getImage().getScaledInstance(250, 350, Image.SCALE_SMOOTH);
         ImageIcon image = new ImageIcon(resized);
 
-        //JLabel imgLabel = new JLabel();
         JLabel label = new JLabel();
-        label.setFont(new Font(null,Font.PLAIN,25));
+        label.setFont(new Font(null,Font.PLAIN,20));
         label.setText(title);
         label.setIcon(image);
         label.setSize(200, 300);
         label.setHorizontalTextPosition(JLabel.CENTER);
         label.setVerticalTextPosition(JLabel.BOTTOM);
 
+
+        p1.setPreferredSize(new Dimension(400,500));
         //Add to Panel
         p1.add(label);
+
         p1.add(b);
+        mainP.add(p1);
+
 
         // Add to Frame
 
