47e38ffe445549cbbe321ca96ce49a16b9df071e
Eunil <eunilcarl02@gmail.com>
2024-06-18 15:47:13 +0800

    Updated Concert File

--- src/Concert.java
+++ src/Concert.java
@@ -5,46 +5,63 @@ import java.awt.event.ActionListener;
 
 public class Concert extends JFrame implements ActionListener {
     JButton b;
+    JPanel p;
+    JPanel p1;
+    JPanel head;
+    java.util.List<JButton> buttons;
 
     Concert() {
-        this.setLayout(new FlowLayout(FlowLayout.CENTER));
+        head = new JPanel();
+        p = new JPanel();
+        p1 = new JPanel();
+
+        
+        p.setBackground(new Color(204,102,0));
+        p.setPreferredSize(new Dimension(500,800));
+        p.setLayout(new FlowLayout());
+        p.add(p1);
+
+
+        buttons = new java.util.ArrayList<>();
+        this.setLayout(new BorderLayout());
         this.setSize(1900, 1000);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setVisible(true);
-        this.setResizable(false);
+        this.add(p, BorderLayout.SOUTH);
+
     }
 
     public void setComp(String title, String loc) {
+
+
         b = new JButton();
         b.setText("Buy Tickets");
         b.addActionListener(this);
+        b.setFocusable(false);
 
-        JPanel p = new JPanel();
-        p.setBackground(Color.red);
-        p.setBounds(0,0,250,250);
+        buttons.add(b);
+
+
         ImageIcon img = new ImageIcon(loc);
         Image resized = img.getImage().getScaledInstance(200, 250, Image.SCALE_SMOOTH);
         ImageIcon image = new ImageIcon(resized);
 
         //JLabel imgLabel = new JLabel();
         JLabel label = new JLabel();
+        label.setFont(new Font(null,Font.PLAIN,25));
         label.setText(title);
         label.setIcon(image);
         label.setSize(200, 300);
         label.setHorizontalTextPosition(JLabel.CENTER);
         label.setVerticalTextPosition(JLabel.BOTTOM);
 
-        // Add to Label
-
+        //Add to Panel
+        p1.add(label);
+        p1.add(b);
 
         // Add to Frame
-        this.add(label);
-        this.add(b);
 
-
-
-
-        // Revalidate and repaint the frame to update the UI
+        //to update the UI
         this.revalidate();
         this.repaint();
 
@@ -52,8 +69,13 @@ public class Concert extends JFrame implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if(e.getSource()==b){
-            new Buy();
+        for(JButton button : buttons){
+            if(e.getSource()== button){
+                this.dispose();
+                new Buy();
+                break;
+            }
         }
+
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,13 @@ public class Main{
     public static void main (String[] args){
 
        Concert c = new Concert();
+        c.setComp("Bini", "img/cat.jpg");
+        c.setComp("BPSU", "img/wa.jpg");
         c.setComp("biniasdasd", "img/wa.jpg");
-        c.setComp("biniasdasd", "img/wa.jpg");
-        c.setComp("biniasdasd", "img/wa.jpg");
+        c.setComp("justin bieber", "img/wa.jpg");
+
+
+
 
 
 
